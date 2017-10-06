08a5a8f1ec83eaaa264a693376cde02af8f42b16
Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>
2017-10-07 02:49:53 +0800

    added checks
    Signed-off-by: Toni-Jan Keith Monserrat <tonijanmonserrat@gmail.com>

--- src/modules/nice-module/pages/login-page/login-page.js
+++ src/modules/nice-module/pages/login-page/login-page.js
@@ -2,6 +2,7 @@ import 'polymer/polymer.html'
 import 'paper-button/paper-button.html'
 import 'paper-material/paper-material.html'
 import User from '../../components/user-model/user-model.js'
+import Permission from '../../components/permission-model/permission-model.js'
 import './login-page.html'
 
 class LoginPage extends User(Polymer.Element) {
@@ -9,14 +10,20 @@ class LoginPage extends User(Polymer.Element) {
 
   static get observers () {
     return [
-      '_checkUser(user, profile, profile.agree)'
+      '_checkUser(user, permission.role, profile, profile.agree)'
     ]
   }
 
-  _checkUser (user, profile, agree) {
+  _checkUser (user, role, profile, agree) {
     if (user && profile) {
       if (!agree) {
         window.history.pushState({}, '', '/community/' + this.user.uid + '/edit')
+        window.dispatchEvent(new CustomEvent('location-changed'))
+      } else if (!this._checkRole(user, role, 'member')) {
+        window.history.pushState({}, '', '/community/' + this.user.uid)
+        window.dispatchEvent(new CustomEvent('location-changed'))
+      } else {
+        window.history.pushState({}, '', '/community/')
         window.dispatchEvent(new CustomEvent('location-changed'))
       }
     }
